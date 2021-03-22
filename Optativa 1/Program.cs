c9fffc17e45b323b9e3ab75207ee18ea3b66cce8
edwinpalacios26 <54686508+edwinpalacios26@users.noreply.github.com>
2021-03-21 19:34:33 -0600

    Upload

---
+++ Optativa 1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           int sum = 0;
+            bool tocontinue = true;
+
+            do
+            {
+                Console.Write("Ingrese el rango: ");
+                int range = int.Parse(Console.ReadLine());
+
+                for(int i = 0; i < range; i++)
+                {
+                    Console.Write("Ingrese el numero:");
+                    int number = Int32.Parse(Console.ReadLine());
+                    sum += number;
+                }
+
+                Console.WriteLine("Promedio: " + sum/range);
+
+                Console.WriteLine("Desea continuar? s/n");
+                char opc = char.Parse(Console.ReadLine());
+                if(opc == 'n')
+                tocontinue = false;
+
+            }while(tocontinue);
+
+
+
+        }
+    }
+}
---
+++ Optativa 3/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Structs
+{
+    class Program
+    {
+        //struct nodes
+        struct Birthday
+        {
+            public int day;
+            public int month;
+            public int year;
+
+
+        }
+        static void Main(string[] args)
+        {
+            //birthday is 31/5/1999
+            Birthday FirstBirthday;
+
+            Console.Write("Enter day: ");
+            FirstBirthday.day = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter month: ");
+            FirstBirthday.month = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter year: ");
+            FirstBirthday.year = int.Parse(Console.ReadLine());
+
+            //validation of data
+            if(FirstBirthday.day == 31 && FirstBirthday.month == 5 && FirstBirthday.year == 1999)
+            
+                    Console.WriteLine("Happy Birthday!");
+            else
+                Console.WriteLine("Sorry it is not your birthday");
+        }
+    }
+}
